73cec52a00845ab53dea3990cbb442fb1a7a0570
kshitij <rajkshitij99@gmail.com>
2025-02-25 13:02:40 +0530

    passed user and job id

--- models/applicationModel.js
+++ models/applicationModel.js
@@ -2,8 +2,16 @@ const mongoose = require("mongoose");
 
 const applicationSchema = new mongoose.Schema(
   {
-    userRefId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true }, // have to change user model name accordingly(arya) 
-    jobRefId: { type: mongoose.Schema.Types.ObjectId, ref: "Job", required: true },
+    userRefId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    jobRefId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Job",
+      required: true,
+    },
     name: { type: String, required: true, trim: true },
     designation: { type: String, required: true, trim: true },
     experience: { type: Number, required: true, min: 0 },
@@ -12,14 +20,18 @@ const applicationSchema = new mongoose.Schema(
     shortlisted: { type: Boolean, default: false },
     profileImage: { type: String, trim: true },
     resumeUrl: { type: String, trim: true },
+<<<<<<< HEAD
     screeningStatus: { type: String, enum: ["Pending", "Passed", "Failed"], default: "Pending" },
     interviewScheduled: {type: Boolean, default: false},
     interviewDate: {type: Date}
 },
 
 { timestamps: true } 
+=======
+  },
+>>>>>>> ce0967b (passed user and job id)
 
+  { timestamps: true }
 );
 
 module.exports = mongoose.model("Application", applicationSchema);
-
